66474bb4fc3b115b17638de451d3fa3e9a3b1f1c
root <root@servauth4.fysik.dtu.dk>
2020-11-10 13:51:57 +0100

    New script

--- roles/printerstatus/files/printerreport.sh
+++ roles/printerstatus/files/printerreport.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-/usr/local/bin/printerstatus.sh b307-m830 ly307-225-co1 ly307-225-bw1 b307-m602 b311-4525 |& mail -s 'Fysik B307/311 printers status' henning@fysik.dtu.dk
-/usr/local/bin/printerstatus.sh ly307-001-bw1 LY311-070-CO1 |& mail -s 'Fysik B307/311 printers status' ljcoe@dtu.dk
-/usr/local/bin/printerstatus.sh cryoprt b309-4525 ws4525 ppfe-5550dn |& mail -s 'Fysik B309 printers status' snim@fysik.dtu.dk
-/usr/local/bin/printerstatus.sh ipf8300 ly309-142-co1 b309-m577 |& mail -s 'Fysik B309 printers status' maje@fysik.dtu.dk
-/usr/local/bin/printerstatus.sh case4015 ly307-east-co1 b312-m725 cinf4015 b312-m553 |& mail -s 'Fysik B307/312 printers status' bpkn@fysik.dtu.dk
+/usr/local/bin/printerstatus.sh -m henning@fysik.dtu.dk	b307-m830 ly307-225-co1 ly307-225-bw1 b307-m602 b311-4525 
+/usr/local/bin/printerstatus.sh -m ljcoe@dtu.dk		ly307-001-bw1 LY311-070-CO1 
+/usr/local/bin/printerstatus.sh -m snim@fysik.dtu.dk	cryoprt b309-4525 ws4525 ppfe-5550dn 
+/usr/local/bin/printerstatus.sh -m maje@fysik.dtu.dk	ipf8300 ly309-142-co1 b309-m577 
+/usr/local/bin/printerstatus.sh -m bpkn@fysik.dtu.dk	case4015 ly307-east-co1 b312-m725 cinf4015 b312-m553 
