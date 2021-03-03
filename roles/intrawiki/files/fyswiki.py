3cfe93c0a7171d0defcc83dc555ae774bb8b1005
root <root@servauth4.fysik.dtu.dk>
2021-03-03 14:06:13 +0100

    File renamed

---
+++ roles/intrawiki/files/fyswiki.py
@@ -0,0 +1,45 @@
+# -*- coding: iso-8859-1 -*-
+# IMPORTANT! This encoding (charset) setting MUST be correct! If you live in a
+# western country and you don't know that you use utf-8, you probably want to
+# use iso-8859-1 (or some other iso charset). If you use utf-8 (a Unicode
+# encoding) you MUST use: coding: utf-8
+# That setting must match the encoding your editor uses when you modify the
+# settings below. If it does not, special non-ASCII chars will be wrong.
+
+"""
+This is a sample config for a wiki that is part of a wiki farm and uses
+farmconfig for common stuff. Here we define what has to be different from
+the farm's common settings.
+"""
+
+# we import the FarmConfig class for common defaults of our wikis:
+from farmconfig import FarmConfig
+
+# now we subclass that config (inherit from it) and change what's different:
+class Config(FarmConfig):
+
+    # basic options (you normally need to change these)
+    sitename = u'FYSwiki' # [Unicode]
+    interwikiname = u'FYSwiki' # [Unicode]
+
+    # name of entry page / front page [Unicode], choose one of those:
+
+    # a) if most wiki content is in a single language
+    #page_front_page = u"MyStartingPage"
+
+    # b) if wiki content is maintained in many languages
+    page_front_page = u"fyswiki"
+
+    # Must NOT be under the web-server tree /var/www, see docs/INSTALL.html
+    data_dir = '/var/moin/fyswiki/data/'
+
+    # The default theme anonymous or new users get
+    theme_default = 'modern'
+    # Use Restructured text as default markup
+    # default_markup = 'rst'
+
+    # Allow read access ONLY to the AdminGroup
+    acl_rights_before = u"AdminGroup:read,write,delete,revert,admin"
+    # acl_rights_before = u"OleHolmNielsen:read,write,delete,revert,admin"
+    acl_rights_default = u"All:read,write"
+    acl_rights_after = u"ReadGroup:read"
---
+++ roles/intrawiki/files/moin.cgi
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - CGI/FCGI Driver script
+
+    @copyright: 2000-2005 by Juergen Hermann <jh@web.de>,
+                2008 by MoinMoin:ThomasWaldmann,
+                2008 by MoinMoin:FlorianKrupicka
+    @license: GNU GPL, see COPYING for details.
+"""
+
+import sys, os
+
+# Addition at fysik.dtu.dk:
+sys.path.insert(0, '/var/www/wiki')
+from MoinMoin import log
+log.load_config('/usr/share/moin/config/logging/stderr')
+
+# this works around a bug in flup's CGI autodetection (as of flup 1.0.1):
+os.environ['FCGI_FORCE_CGI'] = 'Y' # 'Y' for (slow) CGI, 'N' for FCGI
+
+from MoinMoin.web.flup_frontend import CGIFrontEnd
+CGIFrontEnd().run()
+
